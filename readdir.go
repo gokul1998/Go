7da33aff20fe4ed554f6faa290a381d19b12dc06
Gokulakrishnan <gokulakrishnan.parir@fireeye.com>
2019-07-17 10:54:19 +0530

    path wrong

--- readdir.go
+++ readdir.go
@@ -5,20 +5,20 @@ import (
 	"io/ioutil"
 )
 
-func fileexists(path, file string) bool {
+func fileexists(path, file string) (bool, string) {
 	c, err := ioutil.ReadDir(path)
 	if err != nil {
 		fmt.Println(err)
-		return false
+		return false, " "
 	}
 	for _, v := range c {
 		if v.IsDir() == true && fileexists(path+string('/')+v.Name(), file) == true {
-			return true
+			return false, path + string('/') + v.Name()
 		} else if v.IsDir() == false && v.Name() == file {
-			return true
+			return false, path
 		}
 	}
-	return false
+	return false, " "
 }
 func main() {
 	var p, f string
@@ -26,9 +26,9 @@ func main() {
 	fmt.Scan(&p)
 	fmt.Println("enter the file to check : ")
 	fmt.Scan(&f)
-	res := fileexists(p, f)
+	res, path := fileexists(p, f)
 	if res == true {
-		fmt.Println("file found")
+		fmt.Println("file found", path)
 	} else {
 		fmt.Println("file not found")
 	}
